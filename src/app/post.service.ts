a47e0d7984df1bfdc9684324bf40d6b661c3cd66
karima-elhachimi <yuakarima@Yuas-MBP.lan>
2018-03-07 12:38:30 +0100

    added routing

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { PortfolioItem } from './portfolio-item';
-import {TumblrConnections} from '../tumblr-api-connections';
+import { TumblrConnections } from './tumblr-connections';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import 'rxjs/add/observable/from';
@@ -20,7 +20,7 @@ export class PostService {
   constructor (private http: HttpClient ) { }
 
   getPhotos() {
-    return this.http.get(this.conn.getPhotoPostsUrl)
+    return this.http.get(this.conn.url)
       .map(response => {
         return (<PortfolioItem[]>response)['response']['posts'];
       });
